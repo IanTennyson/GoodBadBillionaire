6628554ee1655f3351c0187c1f49ee18f5854f5c
IanTennyson-CI <ian@continuum.industries>
2025-03-11 17:27:51 +0000

    add font

--- src/App.vue
+++ src/App.vue
@@ -45,21 +45,33 @@ onMounted(async () => {
     </header>
 
     <main>
-      <div>
-        <DataTable
-          :value="billionaires"
-          tableStyle="min-width: 50rem"
-        >
-          <Column field="id" header="ID"></Column>
-          <Column field="name" header="Name"></Column>
-          <Column field="gbb.timeOfRecording.netWorth" header="Net Worth (B$)"></Column>
-          <Column field="industry" header="Industry"></Column>
-        </DataTable>
-      </div>
+      <div class="content">
+        <h1 class="text-3xl text-9xl text-center">GOOD BAD BILLIONAIRE</h1>
 
-      <div ref="chartContainer" style="width: 100%; height: 400px; margin-top: 20px;"></div>
+        <div>
+          <DataTable
+            :value="billionaires"
+            tableStyle="min-width: 50rem"
+          >
+            <Column field="id" header="ID"></Column>
+            <Column field="name" header="Name"></Column>
+            <Column field="gbb.timeOfRecording.netWorth" header="Net Worth (B$)"></Column>
+            <Column field="industry" header="Industry"></Column>
+          </DataTable>
+        </div>
+
+
+        <div ref="chartContainer" style="width: 80%; height: 400px; margin-top: 20px;"></div>
+      </div>
     </main>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.content {
+  margin: 20px;
+  padding: 5%;
+  border: 3px solid #320a2d;
+  background-color: #f3ecdd;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        regular: ["TTDirectorsRegular", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
